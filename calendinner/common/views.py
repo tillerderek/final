47f1639434f8e11b9dbf6e944355785a7bed0a07
derek <tillerderek@gmail.com>
2024-03-10 22:04:37 -0400

    stopping for now

--- calendinner/common/views.py
+++ calendinner/common/views.py
@@ -9,12 +9,24 @@ from recipes.models import Ingredient
 
 def home(request):
     recipes = Recipe.objects.all()
-    
     context = {
         'recipes': recipes,
         'title': 'Dinner\'s Ready',
+        
     }
     return render(request, 'common/home.html', context)
+  
+def search(request):
+    query = request.GET.get('term', '')
+    recipes = Recipe.objects.filter(title__icontains=query)
+    # ingredients = Recipe.objects.filter(ingredients__icontains=query)
+    context = {
+        'recipes': recipes,
+        # 'ingredients': ingredients,
+        'title': 'Search results',
+        'query': query,
+    }
+    return render(request, 'common/search.html', context)
   
   
 def autocomplete(request):
